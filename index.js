9ab132206fd327bcc06109b6164170cf3efc3732
Levy <edgarlevy81@gmail.com>
2023-07-06 17:23:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+
+const random = Number.isSafeInteger(5);
+console.log(random);
+
+const num3 = 2;
+const num4 = 2;
+const mod = num3 + num4
+
+const max = (Math.max(10, 20, 15));
+console.log(max);
